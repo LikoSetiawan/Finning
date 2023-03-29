01d39fcb30f004d39d46a2d614e1bf04914b890f
LikoSetiawan <128354782+LikoSetiawan@users.noreply.github.com>
2023-03-30 00:06:09 +0700

    Add files via upload

---
+++ Liko_TA/Liko_TA/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Liko_TA
+//
+//  Created by Liko Setiawan on 17/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ Liko_TA/Liko_TA/Liko_TAApp.swift
@@ -0,0 +1,17 @@
+//
+//  Liko_TAApp.swift
+//  Liko_TA
+//
+//  Created by Liko Setiawan on 17/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Liko_TAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
